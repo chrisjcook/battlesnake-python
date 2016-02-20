0d3261d8216c030a5e27cf21860f87e916abd356
ben-cunningham <bencunningham17@gmail.com>
2016-02-20 15:18:07 -0800

    Add weights to potential moves

--- app/dfs.py
+++ app/dfs.py
@@ -16,7 +16,7 @@ def dfs(board, our_head, food):
     move = ''
     for coord in food:
         head = our_head[:]
-        distance, moves = __do_search(head, coord)
+        distance, moves = __do_search(board, head, coord)
         if distance < board:
             smallest = distance
             move = moves[0]
@@ -25,33 +25,66 @@ def dfs(board, our_head, food):
     return move
 
 # [2,8], [0,7]
-def __do_search(our_head, item):
+def __do_search(board, our_head, item):
     distance = 0
     current_coord = our_head
     moves = []
     while not __is_equal(current_coord, item):
-        potential_moves = ['north', 'south', 'east', 'west']
+
+        potential_moves = {
+            'north' : 1.0,
+            'south' : 1.0,
+            'east' : 1.0,
+            'west' : 1.0
+        }
+
         x_delta = current_coord[0] - item[0]
         if not x_delta == 0:
+            if board[current_coord[0] + 1][current_coord[1]] == 4:
+                __increase_weight(potential_moves, 'east', 0)
             if x_delta < 0: # go right/east
                 current_coord[0] += 1
-                moves.append('east')
+                __increase_weight(potential_moves, 'east', 2)
             else: # go left/west
                 current_coord[0] -= 1
-                moves.append('west')
+                __increase_weight(potential_moves, 'west', 2)
         else:
-            y_delta = current_coord[1] - item[1]
+            __increase_weight(potential_moves, 'east', 0.5)
+            __increase_weight(potential_moves, 'west', 0.5)
+
+        y_delta = current_coord[1] - item[1]
+        if not y_delta == 0:
+
             if y_delta < 0: # go down/south
                 current_coord[1] += 1
-                moves.append('south')
+                __increase_weight(potential_moves, 'south', 2)
             else: # go up/north
                 current_coord[1] -= 1
-                moves.append('north')
+                __increase_weight(potential_moves, 'north', 2)
 
-        print current_coord
+        # print current_coord
+        move = __get_best_move(potential_moves)
+        moves.append(move)
         distance += 1
 
     return distance, moves
+
+# def __find_enemy_snakes(board):
+
+
+def __increase_weight(weights, direction, factor):
+    weights[direction] *= factor
+
+def __get_best_move(weights):
+    best = weights['north']
+    best_key = 'north'
+    for key in weights:
+        if weights[key] > best:
+            best = weights[key]
+            best_key = key
+
+    return best_key
+
 
 def __is_equal(a,b):
     if a[0] == b[0] and a[1] == b[1]:
