176baa2325aae3a1edc15be2f53c21dd281de167
ben-cunningham <bencunningham17@gmail.com>
2016-02-20 14:34:34 -0800

    Get move for closest food item

--- app/dfs.py
+++ app/dfs.py
@@ -7,23 +7,53 @@ possible_directions = {
     'w' : 'west'
 }
 
+# [0,0]
+#  x y
+
 def dfs(board, our_head, food):
-    distances = []
+    # distances = []
+    smallest = len(board)
+    move = ''
     for coord in food:
-        distance = __do_search(our_head, coord)
-        distances.append(distance)
+        head = our_head[:]
+        distance, moves = __do_search(head, coord)
+        if distance < board:
+            smallest = distance
+            move = moves[0]
+            print distance
+        # distances.append(distance)
+    return move
 
-    return possible_directions[n]
-
+# [2,8], [0,7]
 def __do_search(our_head, item):
     distance = 0
     current_coord = our_head
-    queue = []
-    queue.insert(0, our_head)
-    # while not len(queue) is 0:
+    moves = []
+    while not __is_equal(current_coord, item):
+        potential_moves = ['north', 'south', 'east', 'west']
+        x_delta = current_coord[0] - item[0]
+        if not x_delta == 0:
+            if x_delta < 0: # go right/east
+                current_coord[0] += 1
+                moves.append('east')
+            else: # go left/west
+                current_coord[0] -= 1
+                moves.append('west')
+        else:
+            y_delta = current_coord[1] - item[1]
+            if y_delta < 0: # go down/south
+                current_coord[1] += 1
+                moves.append('south')
+            else: # go up/north
+                current_coord[1] -= 1
+                moves.append('north')
 
+        print current_coord
+        distance += 1
 
-    return distance
+    return distance, moves
 
 def __is_equal(a,b):
-    if a[0] == b[0] and
+    if a[0] == b[0] and a[1] == b[1]:
+        return True
+    return False
