43ba7ecd2d471877dcb15ec99542b8d7a0e60aa4
dustinchang <dustinlchang@gmail.com>
2016-02-20 13:22:12 -0800

    testing health2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def get_health(data):
     for snake in data.get('snakes'):
         if snake.get('id') == '9fccbadb-30bc-4f6e-845f-057e1ea32975':
             return snake.get('health')
-    return 100
+    return 90
 
 @bottle.route('/static/<path:path>')
 def static(path):
