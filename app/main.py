6fcc401ee397491e2af07541cae1a5ca7ce9ee5e
dustinchang <dustinlchang@gmail.com>
2016-02-20 13:28:09 -0800

    testing health2 l_comprehension

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import bottle
 import os
 
 def get_health(data):
-    for snake in data.get('snakes'):
-        if snake.get('id') == '9fccbadb-30bc-4f6e-845f-057e1ea32975':
-            return snake.get('health')
-    return 90
+    return [snake.get('health') for snake in data.get('snakes') if snake.get('id') == '9fccbadb-30bc-4f6e-845f-057e1ea32975'][0]
+
+
+    for snake in data.get()
 
 @bottle.route('/static/<path:path>')
 def static(path):
