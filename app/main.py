0776e2fdec2539464a00792afb91c8f329fb1f40
dustinchang <dustinlchang@gmail.com>
2016-02-20 17:18:22 -0800

    might circle

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import bottle
 import os
+import math
+
+board = []
+board_width = 0
+board_height = 0
 
 def get_health(data):
     for snake in data.get('snakes'):
@@ -76,6 +81,9 @@ def mid_game(data):
     }
 
 def end_game(data):
+    global board
+    global board_width
+    global board_height
     if not target_enemy(data):
         #go for food
         pass
@@ -86,13 +94,82 @@ def end_game(data):
         else:
             #ATTACK!!!
             my_head, my_body, target_head, target_body = determine_position(data)
-            #globals
-
-
+            grid_list = get_grid_line()
+            quadrant_coord = get_quadrant(my_head) #quadrant_coord your in
+            if my_head in grid_list:
+                index = grid_list.index(my_head)
+                if index == len(grid_list) -1:
+                    next_coord == grid_list[0]
+                else:
+                    next_coord = grid_list[index + 1]
+                    x_val = next_coord[0] - my_head[0]
+                    y_val = next_coord[1] - my_head[1]
+                    if x_val != 0:
+                        #moving next dir
+                        if x_val > 0:
+                            return {'move': 'east'}
+                        else:
+                            return {'move': 'west'}
+                    else:
+                        if y_val > 0:
+                            return {'move': 'south'}
+                        else:
+                            return {'move': 'north'}
+            else:
+                x_val = quadrant_coord[0] - my_head[0]
+                y_val = quadrant_coord[1] - my_head[1]
+                x_min_dist = abs(x_val)
+                y_min_dist = abs(y_val)
+                if x_min_dist < y_min_dist:
+                    if x_val < 0:
+                        #move right
+                        return {'move': 'west'}
+                    else:
+                        return {'move': 'east'}
+                else:
+                    if y_val < 0:
+                        return {'move': 'up'}
+                    else:
+                        return {'move': 'down'}
     return {
         'move': 'north',
         'taunt': 'ARBOK!'
     }
+
+def get_quadrant(my_head):
+    top_left = math.sqrt((my_head[0]-2)**2 + (my_head[1] - 2)**2)
+    top_right = math.sqrt((my_head[0]-board_width-2)**2 + (my_head[1] - 2)**2)
+    bottom_left = math.sqrt((my_head[0]-2)**2 + (my_head[1] - board_height-2)**2)
+    bottom_right = math.sqrt((my_head[0] - board_width-2)**2 + (my_head[1] - board_height-2)**2)
+    smallest = min(top_left, top_right, bottom_left, bottom_right)
+    if smallest == top_left:
+        return [2, 2]
+    elif smallest == top_right:
+        return [board_width-2, 2]
+    elif smallest == bottom_left:
+        return [2, board_height-2]
+    elif smallest == bottom_right:
+        return [board_width-2, board_height-2]
+
+
+
+def get_grid_line():
+    global board
+    global board_width
+    global board_height
+    y_values = range(2, board_height-2)
+    x_values = range(3, board_width-3)
+    grid_1 = []
+    for y in y_values:
+        #west and east lines of board
+        grid_1.append([2, y])
+        grid_2.appead([board_width-2, y])
+    for x in x_values:
+        grid_3.appead(x, 2)
+        grid_4.appead(x, board_height-2)
+    grid_1.extend(grid_4+grid_2+grid_3)
+    return grid_1
+
 
 def determine_position(data):
     my_head = []
